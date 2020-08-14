312b50ec518f0afad7f9729ff7cbc405e363e080
Draqqonfly <dragovonnova@gmail.com>
2020-08-14 18:27:43 +0200

    Some practise with resources

--- app/src/main/java/com/example/randomlearning/MainActivity.kt
+++ app/src/main/java/com/example/randomlearning/MainActivity.kt
@@ -1,26 +1,31 @@
 package com.example.randomlearning
 
-import android.content.Intent
+import android.content.res.TypedArray
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import java.lang.ref.PhantomReference
-import java.lang.ref.SoftReference
-import java.lang.ref.WeakReference
+import androidx.annotation.RequiresApi
+
+fun print(any: Any) = Log.v("Testy", any.toString())
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.button).setOnClickListener {
-            startActivity(Intent(this, SecondActivity::class.java))
-            finish()
-            System.gc()
+        setContentView(R.layout.test_layout)
+        //if(R.bool.niewiem)
+        if (getDrawable(R.drawable.person) == getDrawable(R.drawable.person_pl)) {
+            print("Persony są te same! Użyto aliasu")
         }
-        if (!ReferenceHolder.set) {
-            ReferenceHolder.setReference(this)
-        }
-        ReferenceHolder.printReferences(this)
+        //<-- Look at colors on the left!
+        R.color.colorPrimary
+        R.color.alias
+
+        if (!resources.getBoolean(R.bool.niewiem)) print("Już wiem!")
+
+        val array: TypedArray = resources.obtainTypedArray(R.array.random_array)
+        array.recycle()
+
     }
 }
