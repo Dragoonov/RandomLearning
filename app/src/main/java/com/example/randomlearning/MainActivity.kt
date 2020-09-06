8faabeeca57ee7961eb0447e22a0690e891e4887
Draqqonfly <dragovonnova@gmail.com>
2020-09-06 18:26:41 +0200

    Some practise with executors

--- app/src/main/java/com/example/randomlearning/MainActivity.kt
+++ app/src/main/java/com/example/randomlearning/MainActivity.kt
@@ -4,34 +4,47 @@ import android.os.*
 import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import androidx.annotation.RequiresApi
+import androidx.core.os.HandlerCompat
 import androidx.core.os.postDelayed
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 fun printMessage(message: Any?) = Log.v(MainActivity::class.simpleName, message.toString())
-
-class MyLooperThread : Thread("My looped thread") {
-    var handler: Handler? = null
-
-    override fun run() {
-        Looper.prepare()
-        handler = object : Handler() {
-            override fun handleMessage(msg: Message) {
-                printMessage("Message handled: $msg")
-            }
-
-            override fun dispatchMessage(msg: Message) {
-                printMessage("Message dispatched: $msg")
-                super.dispatchMessage(msg)
-            }
-        }
-        Looper.loop()
-    }
-}
 
 class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //handlersTesting()
+        executorsHandlers()
+    }
+
+    private fun executorsHandlers() {
+        val executorService = Executors.newFixedThreadPool(5)
+        val mainThreadHandler = Handler(Looper.getMainLooper())
+        val backgroundHandlerThread = HandlerThread("background thread")
+        backgroundHandlerThread.start()
+        val backgroundHandler = Handler(backgroundHandlerThread.looper)
+        backgroundHandler.post {
+            printMessage("Executing on a backgroundHandler:" + Thread.currentThread())
+            mainThreadHandler.post {
+                printMessage("And now switched to main (backgroundHandler): " + Thread.currentThread())
+            }
+            printMessage("And again on backgroundHandler:" + Thread.currentThread())
+        }
+        executorService.execute {
+            printMessage("Executing on a " + Thread.currentThread())
+            mainThreadHandler.post {
+                printMessage("And now switched to main: " + Thread.currentThread())
+            }
+            printMessage("And again on " + Thread.currentThread())
+        }
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+    private fun handlersTesting() {
         val runnable = Runnable {
             Thread.sleep(250)
             printMessage("Created runnable, in a thread: " + Thread.currentThread().name)
@@ -63,5 +76,24 @@ class MainActivity : AppCompatActivity() {
         handlerThread.quitSafely()
         looperThread.handler!!.looper.quitSafely()
         Thread.sleep(500)
+    }
+
+    class MyLooperThread : Thread("My looped thread") {
+        var handler: Handler? = null
+
+        override fun run() {
+            Looper.prepare()
+            handler = object : Handler() {
+                override fun handleMessage(msg: Message) {
+                    printMessage("Message handled: $msg")
+                }
+
+                override fun dispatchMessage(msg: Message) {
+                    printMessage("Message dispatched: $msg")
+                    super.dispatchMessage(msg)
+                }
+            }
+            Looper.loop()
+        }
     }
 }
