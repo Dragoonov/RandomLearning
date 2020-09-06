db356a2283824eb4488d1e00b8768c632968713e
Draqqonfly <dragovonnova@gmail.com>
2020-09-06 21:28:57 +0200

    Basic service implementation

---
+++ app/src/main/java/com/example/randomlearning/ExampleService.kt
@@ -0,0 +1,102 @@
+package com.example.randomlearning
+
+import android.app.*
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.*
+import android.os.Process.THREAD_PRIORITY_BACKGROUND
+import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+
+class ExampleService: Service() {
+    private var serviceLooper: Looper? = null
+    private var serviceHandler: ServiceHandler? = null
+
+    // Handler that receives messages from the thread
+    private inner class ServiceHandler(looper: Looper) : Handler(looper) {
+
+        override fun handleMessage(msg: Message) {
+            // Normally we would do some work here, like download a file.
+            // For our sample, we just sleep for 5 seconds.
+            try {
+                Log.v("ExampleService ", "Started, Argument 1: " + msg.arg1)
+                Thread.sleep(5000)
+                Log.v("ExampleService ", "Stopped, Argument 1: " + msg.arg1)
+            } catch (e: InterruptedException) {
+                // Restore interrupt status.
+                Thread.currentThread().interrupt()
+            }
+
+            // Stop the service using the startId, so that we don't stop
+            // the service in the middle of handling another job
+            stopSelf(msg.arg1)
+        }
+    }
+
+    override fun onCreate() {
+        // Start up the thread running the service.  Note that we create a
+        // separate thread because the service normally runs in the process's
+        // main thread, which we don't want to block.  We also make it
+        // background priority so CPU-intensive work will not disrupt our UI.
+        HandlerThread("ServiceStartArguments", THREAD_PRIORITY_BACKGROUND).apply {
+            start()
+
+            // Get the HandlerThread's Looper and use it for our Handler
+            serviceLooper = looper
+            serviceHandler = ServiceHandler(looper)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(channelId: String, channelName: String): String{
+        val chan = NotificationChannel(channelId,
+            channelName, NotificationManager.IMPORTANCE_NONE)
+        chan.lightColor = Color.BLUE
+        chan.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+        val service = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        service.createNotificationChannel(chan)
+        return channelId
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show()
+
+        // For each start request, send a message to start a job and deliver the
+        // start ID so we know which request we're stopping when we finish the job
+        serviceHandler?.obtainMessage()?.also { msg ->
+            msg.arg1 = startId
+            serviceHandler?.sendMessage(msg)
+        }
+
+        val pendingIntent: PendingIntent =
+            Intent(this, MainActivity::class.java).let { notificationIntent ->
+                PendingIntent.getActivity(this, 0, notificationIntent, 0)
+            }
+
+        val channel = createNotificationChannel("my_service", "My Background Service")
+
+        val notification: Notification = Notification.Builder(this, channel)
+            .setContentTitle("Some title")
+            .setContentText("some text")
+            .setContentIntent(pendingIntent)
+            .build()
+
+        startForeground(startId, notification)
+
+        // If we get killed, after returning from here, restart
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        // We don't provide binding, so return null
+        return null
+    }
+
+    override fun onDestroy() {
+        Toast.makeText(this, "service done", Toast.LENGTH_SHORT).show()
+    }
+}
--- app/src/main/java/com/example/randomlearning/MainActivity.kt
+++ app/src/main/java/com/example/randomlearning/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.randomlearning
 
+import android.content.Intent
 import android.os.*
 import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import androidx.annotation.RequiresApi
-import androidx.core.os.HandlerCompat
-import androidx.core.os.postDelayed
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 
 fun printMessage(message: Any?) = Log.v(MainActivity::class.simpleName, message.toString())
 
@@ -16,84 +13,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //handlersTesting()
-        executorsHandlers()
-    }
-
-    private fun executorsHandlers() {
-        val executorService = Executors.newFixedThreadPool(5)
-        val mainThreadHandler = Handler(Looper.getMainLooper())
-        val backgroundHandlerThread = HandlerThread("background thread")
-        backgroundHandlerThread.start()
-        val backgroundHandler = Handler(backgroundHandlerThread.looper)
-        backgroundHandler.post {
-            printMessage("Executing on a backgroundHandler:" + Thread.currentThread())
-            mainThreadHandler.post {
-                printMessage("And now switched to main (backgroundHandler): " + Thread.currentThread())
-            }
-            printMessage("And again on backgroundHandler:" + Thread.currentThread())
-        }
-        executorService.execute {
-            printMessage("Executing on a " + Thread.currentThread())
-            mainThreadHandler.post {
-                printMessage("And now switched to main: " + Thread.currentThread())
-            }
-            printMessage("And again on " + Thread.currentThread())
-        }
-    }
-
-
-    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
-    private fun handlersTesting() {
-        val runnable = Runnable {
-            Thread.sleep(250)
-            printMessage("Created runnable, in a thread: " + Thread.currentThread().name)
-        }
-        val notLoopedThread = Thread(runnable,"NotLoopedThread")
-        val looperThread = MyLooperThread()
-        val handler = Handler()
-        looperThread.start()
-        notLoopedThread.start()
-        Thread.sleep(200) // Wait for loopedThread to initialize looper
-        handler.postDelayed(300) { printMessage("Printing delayed 300ms: " + Thread.currentThread())}
-        looperThread.handler?.post { printMessage("2 + 2 is 42: " + Thread.currentThread()) }
-        val handler2 = object : Handler(looperThread.handler?.looper!!) {
-            override fun handleMessage(msg: Message) {
-                printMessage(msg.obj as String + " in " + Thread.currentThread())
-            }
-        }
-        val message = Message().also {
-            it.obj = "Custom message"
-        }
-        handler2.dispatchMessage(message)
-        handler2.sendMessage(message)
-        handler2.post { printMessage("3 + 3 is 33: " + Thread.currentThread()) }
-
-        val handlerThread = HandlerThread("Handler Thread")
-        handlerThread.start()
-        val handler3 = Handler(handlerThread.looper)
-        handler3.post { printMessage("Inside a handlerThread!") }
-        handlerThread.quitSafely()
-        looperThread.handler!!.looper.quitSafely()
-        Thread.sleep(500)
-    }
-
-    class MyLooperThread : Thread("My looped thread") {
-        var handler: Handler? = null
-
-        override fun run() {
-            Looper.prepare()
-            handler = object : Handler() {
-                override fun handleMessage(msg: Message) {
-                    printMessage("Message handled: $msg")
-                }
-
-                override fun dispatchMessage(msg: Message) {
-                    printMessage("Message dispatched: $msg")
-                    super.dispatchMessage(msg)
-                }
-            }
-            Looper.loop()
+        Intent(this, ExampleService::class.java).also {
+            startForegroundService(it)
+            startForegroundService(it)
+            startForegroundService(it)
         }
     }
 }
