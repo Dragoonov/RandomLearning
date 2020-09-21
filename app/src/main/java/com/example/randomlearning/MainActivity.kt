dd22b7a70d008c6719ffd7bd3e55245c5e42778c
Dragoonov <dragovonnova@gmail.com>
2020-09-21 20:09:52 +0200

    adapter

--- app/src/main/java/com/example/randomlearning/MainActivity.kt
+++ app/src/main/java/com/example/randomlearning/MainActivity.kt
@@ -8,29 +8,54 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val singleton = Singleton.getSingleton()
-        singleton.value = "Mama"
-        val singleton2 = Singleton.getSingleton()
+        val human = Human()
+        val spider = Tarantula()
+        val adapter = HumanSpiderAdapter(human)
+        val holeForSpider = SpiderHole()
+        val holeForHuman = SpiderHole()
 
-        Log.v("Main", (singleton.value == singleton2.value).toString())
+        holeForSpider.receive(spider)
+        holeForHuman.receive(adapter)
+
+        Log.v("Main", holeForHuman.taken.toString() + " " + holeForSpider.taken.toString())
     }
 }
 
-//Wow, such a huge pattern
-class Singleton private constructor() {
-    var value: String = ""
+interface Spider {
+    fun legSpan(): Int
+}
 
-    companion object {
+class SpiderHole {
 
-        fun getSingleton(): Singleton {
-            return if (instance == null) {
-                instance = Singleton()
-                instance as Singleton
-            } else {
-                instance as Singleton
-            }
-        }
-        private var instance: Singleton? = null
+    var taken = false
+    private val maxLegSpan = 20
+
+    private fun isBigEnough(spider: Spider): Boolean {
+        return spider.legSpan() < maxLegSpan
     }
 
+    fun receive (spider: Spider) {
+        if (isBigEnough(spider) || taken) {
+            taken = true
+        }
+    }
+}
+
+class Tarantula: Spider {
+    private val legLength = 3
+    override fun legSpan() = legLength*8
+}
+
+class Human {
+    val leftHand = 2
+    val rightHand = 4
+    val leftLeg = 6
+    val rightLeg = 6
+}
+
+class HumanSpiderAdapter(private val human: Human): Spider {
+
+    override fun legSpan(): Int {
+        return human.leftLeg + human.rightLeg + human.leftHand + human.rightHand
+    }
 }
