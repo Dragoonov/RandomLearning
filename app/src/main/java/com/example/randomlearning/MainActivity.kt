87805bf7cd01c17b5163d4b519f0dad27f08c983
Dragoonov <dragovonnova@gmail.com>
2020-09-22 20:24:41 +0200

    decorator

--- app/src/main/java/com/example/randomlearning/MainActivity.kt
+++ app/src/main/java/com/example/randomlearning/MainActivity.kt
@@ -8,50 +8,60 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val tree: TreeComponent = Trunk(
-            listOf(
-                Branch(
-                    listOf(Leaf(), Leaf(), Leaf()
-            )), Branch(
-                    listOf(Leaf(), Leaf(), Leaf()))
-            )
-        )
+        val simpleDog: Dog = York()
+        val withWings = DogWithWingsDecorator(simpleDog)
+        val andJumps = JumpingDogDecorator(withWings)
+        val godAlmighty = AlmightyDogDecorator(andJumps)
 
-        Log.v("Composite", tree.getWeight().toString())
+        godAlmighty.bark()
     }
 }
 
-interface TreeComponent {
-    fun getWeight(): Int
+interface Dog {
+    fun bark()
 }
 
-class Trunk(private val components: List<TreeComponent>): TreeComponent {
-
-    private val weight = 5
-
-    override fun getWeight(): Int {
-        return weight + components
-            .map { it.getWeight() }
-            .reduce { acc, treeComponent -> acc+treeComponent }
+class York : Dog {
+    override fun bark() {
+        Log.v("Decorator", "A true bark")
     }
 }
 
-class Branch(private val components: List<TreeComponent>): TreeComponent {
-
-    private val weight = 3
-    override fun getWeight(): Int {
-        return weight + components
-            .map { it.getWeight() }
-            .reduce { acc, treeComponent -> acc+treeComponent }
+abstract class BaseDogDecorator(private val dog: Dog) : Dog {
+    override fun bark() {
+        dog.bark()
     }
 }
 
-class Leaf: TreeComponent {
-    private val weight = 1
-    override fun getWeight(): Int {
-        return weight
+class DogWithWingsDecorator(dog: Dog) : BaseDogDecorator(dog) {
+    override fun bark() {
+        Log.v("Decorator", "Move the wings before the bark")
+        super.bark()
+    }
+}
+
+class JumpingDogDecorator(dog: Dog) : BaseDogDecorator(dog) {
+
+    private fun jump() {
+        Log.v("Decorator", "Jump from joy!")
     }
 
+    override fun bark() {
+        super.bark()
+        jump()
+    }
+}
+
+class AlmightyDogDecorator(dog: Dog) : BaseDogDecorator(dog) {
+
+    private fun killThePlanet() {
+        Log.v("Decorator", "Destroys the earth killing the whole human population")
+    }
+
+    override fun bark() {
+        super.bark()
+        killThePlanet()
+    }
 }
 
 
